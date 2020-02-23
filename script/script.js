d2e040a37e22420f81d3936209a7762de96156c0
smozhevsky <smozhevsky@gmail.com>
2020-02-23 16:13:41 +0300

    add files

---
+++ script/script.js
@@ -0,0 +1,62 @@
+let x = 1;
+let y = 1;
+let deltaX = 1;
+let deltaY = 0;
+let appleX = 1;
+let appleY = 3;
+let counter = 0;
+
+
+// addEventListener
+window.onkeypress = function(event) {
+    // switch-case
+    if (event.key === "w") {
+        deltaX = 0;
+        deltaY = -1;
+    }
+    if (event.key === "d") {
+        deltaX = 1;
+        deltaY = 0;
+    }
+    if (event.key === "a") {
+        deltaX = -1;
+        deltaY = 0;
+    }
+    if (event.key === "s") {
+        deltaX = 0;
+        deltaY = 1;
+    }
+
+    tickLogic();
+} 
+
+
+let tickLogic = function () {
+    // window.x2y2.append(pacman)
+    let newX = x + deltaX;
+    let newY = y + deltaY;
+    if (newX < 4 && newX > 0) {
+        x = newX;
+    }
+    if (newY < 4 && newY > 0) {
+        y = newY;
+    }
+    
+    if (x === appleX && y === appleY) {
+      counter++;
+      score.innerText = counter;
+      while (appleX === x && appleY === y) {
+        appleX = Math.floor(Math.random() * 3) + 1;
+        appleY = Math.floor(Math.random() * 3) + 1;
+      }
+
+      let nextAppleStep = "x" +  appleX + "y" + appleY; // "x1y1"
+      window[nextAppleStep].append(apple);
+    }
+
+
+    let nextStep = "x" + x + "y" + y; // "x1y1"
+    window[nextStep].append(pacman);
+}
+
+// setInterval(tickLogic, 500)
